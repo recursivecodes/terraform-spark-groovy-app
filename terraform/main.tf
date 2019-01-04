b2ac04fedd11c89307ecd7d19c1ffeb363a82d18
Todd Sharp <toddraymondsharp@gmail.com>
2019-01-04 13:46:19 -0500

    update Jenkinsfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-
-provider "oci" {
-  tenancy_ocid = "${var.tenancy_ocid}"
-  user_ocid = "${var.user_ocid}"
-  fingerprint = "${var.fingerprint}"
-  private_key_path = "${var.private_key_path}"
-  private_key_password = "${var.private_key_password}"
-  region = "${var.region}"
-}
-
 data "oci_identity_availability_domains" "ads" {
   compartment_id = "${var.compartment_ocid}"
 }
