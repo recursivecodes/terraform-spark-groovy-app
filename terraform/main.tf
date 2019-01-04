0b331dacd0a6928ee9f0ab7cb6a571983beeb77e
Todd Sharp <toddraymondsharp@gmail.com>
2019-01-04 14:02:47 -0500

    update Jenkinsfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,68 @@
+variable "compartment_ocid" {}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "private_key_password" {}
+variable "region" {}
+variable "ssh_public_key" {}
+variable "ssh_private_key" {}
+
+# Defines the number of instances to deploy
+variable "NumInstances" {
+  default = "3"
+}
+
+# Defines the number of volumes to create and attach to each instance
+# NOTE: Changing this value after applying it could result in re-attaching existing volumes to different instances.
+# This is a result of using 'count' variables to specify the volume and instance IDs for the volume attachment resource.
+variable "NumIscsiVolumesPerInstance" {
+  default = "1"
+}
+
+variable "NumParavirtualizedVolumesPerInstance" {
+  default = "1"
+}
+
+# Choose an Availability Domain
+variable "availability_domain" {
+  default = "3"
+}
+
+variable "instance_shape" {
+  default = "VM.Standard2.1"
+}
+
+variable "instance_image_ocid" {
+  type = "map"
+
+  default = {
+    // See https://docs.us-phoenix-1.oraclecloud.com/images/
+    // Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
+    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaaoqj42sokaoh42l76wsyhn3k2beuntrh5maj3gmgmzeyr55zzrwwa"
+    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaitzn6tdyjer7jl34h2ujz74jwy5nkbukbh55ekp6oyzwrtfa4zma"
+    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaa32voyikkkzfxyo4xbdmadc2dmvorfxxgdhpnk6dw64fa3l4jh7wa"
+  }
+}
+
+variable "DBSize" {
+  default = "50" // size in GBs
+}
+
+variable "BootStrapFile" {
+  default = "./userdata/bootstrap"
+}
+
+variable "tag_namespace_description" {
+  default = "Just a test"
+}
+
+variable "tag_namespace_name" {
+  default = "exampletagns"
+}
+
+
 provider "oci" {
   tenancy_ocid = "${var.tenancy_ocid}"
   user_ocid = "${var.user_ocid}"
@@ -6,3 +71,149 @@ provider "oci" {
   private_key_path = "${var.private_key_path}"
   region = "${var.region}"
 }
+
+# Gets a list of Availability Domains
+data "oci_identity_availability_domains" "ADs" {
+  compartment_id = "${var.tenancy_ocid}"
+}
+
+# Gets the boot volume attachments for each instance
+data "oci_core_boot_volume_attachments" "TFBootVolumeAttachments" {
+  depends_on          = ["oci_core_instance.TFInstance"]
+  count               = "${var.NumInstances}"
+  availability_domain = "${oci_core_instance.TFInstance.*.availability_domain[count.index]}"
+  compartment_id      = "${var.compartment_ocid}"
+
+  instance_id = "${oci_core_instance.TFInstance.*.id[count.index]}"
+}
+
+resource "oci_core_volume" "TFBlock" {
+  count               = "${var.NumInstances * var.NumIscsiVolumesPerInstance}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.availability_domain - 1],"name")}"
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "TFBlock${count.index}"
+  size_in_gbs         = "${var.DBSize}"
+}
+
+resource "oci_core_volume_attachment" "TFBlockAttach" {
+  count           = "${var.NumInstances * var.NumIscsiVolumesPerInstance}"
+  attachment_type = "iscsi"
+  compartment_id  = "${var.compartment_ocid}"
+  instance_id     = "${oci_core_instance.TFInstance.*.id[count.index / var.NumIscsiVolumesPerInstance]}"
+  volume_id       = "${oci_core_volume.TFBlock.*.id[count.index]}"
+
+  # Set this to enable CHAP authentication for an ISCSI volume attachment. The oci_core_volume_attachment resource will
+  # contain the CHAP authentication details via the "chap_secret" and "chap_username" attributes.
+  #use_chap = true
+
+  # Set this to attach the volume as read-only.
+  #is_read_only = true
+}
+
+resource "oci_core_volume" "TFBlockParavirtualized" {
+  count               = "${var.NumInstances * var.NumParavirtualizedVolumesPerInstance}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.availability_domain - 1],"name")}"
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "TFBlockParavirtualized${count.index}"
+  size_in_gbs         = "${var.DBSize}"
+}
+
+resource "oci_core_volume_attachment" "TFBlockAttachParavirtualized" {
+  count           = "${var.NumInstances * var.NumParavirtualizedVolumesPerInstance}"
+  attachment_type = "paravirtualized"
+  compartment_id  = "${var.compartment_ocid}"
+  instance_id     = "${oci_core_instance.TFInstance.*.id[count.index / var.NumParavirtualizedVolumesPerInstance]}"
+  volume_id       = "${oci_core_volume.TFBlockParavirtualized.*.id[count.index]}"
+
+  # Set this to attach the volume as read-only.
+  #is_read_only = true
+}
+resource "oci_identity_tag_namespace" "tag-namespace1" {
+  #Required
+  compartment_id = "${var.compartment_ocid}"
+  description    = "${var.tag_namespace_description}"
+  name           = "${var.tag_namespace_name}"
+}
+
+resource "oci_identity_tag" "tag1" {
+  #Required
+  description      = "tf example tag"
+  name             = "tf-example-tag"
+  tag_namespace_id = "${oci_identity_tag_namespace.tag-namespace1.id}"
+}
+
+resource "oci_identity_tag" "tag2" {
+  #Required
+  description      = "tf example tag 2"
+  name             = "tf-example-tag-2"
+  tag_namespace_id = "${oci_identity_tag_namespace.tag-namespace1.id}"
+}
+
+resource "oci_core_instance" "TFInstance" {
+  count               = "${var.NumInstances}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.availability_domain - 1],"name")}"
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "TFInstance${count.index}"
+  shape               = "${var.instance_shape}"
+
+  create_vnic_details {
+    subnet_id        = "${oci_core_subnet.ExampleSubnet.id}"
+    display_name     = "primaryvnic"
+    assign_public_ip = true
+    hostname_label   = "tfexampleinstance${count.index}"
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = "${var.instance_image_ocid[var.region]}"
+
+    # Apply this to set the size of the boot volume that's created for this instance.
+    # Otherwise, the default boot volume size of the image is used.
+    # This should only be specified when source_type is set to "image".
+    #boot_volume_size_in_gbs = "60"
+  }
+
+  # Apply the following flag only if you wish to preserve the attached boot volume upon destroying this instance
+  # Setting this and destroying the instance will result in a boot volume that should be managed outside of this config.
+  # When changing this value, make sure to run 'terraform apply' so that it takes effect before the resource is destroyed.
+  #preserve_boot_volume = true
+
+  metadata {
+    ssh_authorized_keys = "${var.ssh_public_key}"
+    user_data           = "${base64encode(file(var.BootStrapFile))}"
+  }
+  defined_tags = "${
+    map(
+      "${oci_identity_tag_namespace.tag-namespace1.name}.${oci_identity_tag.tag2.name}", "awesome-app-server"
+    )
+  }"
+  freeform_tags = "${map("freeformkey${count.index}", "freeformvalue${count.index}")}"
+  timeouts {
+    create = "60m"
+  }
+}
+
+# Output the private and public IPs of the instance
+output "InstancePrivateIPs" {
+  value = ["${oci_core_instance.TFInstance.*.private_ip}"]
+}
+
+output "InstancePublicIPs" {
+  value = ["${oci_core_instance.TFInstance.*.public_ip}"]
+}
+
+# Output the boot volume IDs of the instance
+output "BootVolumeIDs" {
+  value = ["${oci_core_instance.TFInstance.*.boot_volume_id}"]
+}
+
+# Output the chap secret information for ISCSI volume attachments. This can be used to output
+# CHAP information for ISCSI volume attachments that have "use_chap" set to true.
+#output "IscsiVolumeAttachmentChapUsernames" {
+#  value = ["${oci_core_volume_attachment.TFBlockAttach.*.chap_username}"]
+#}
+#
+#output "IscsiVolumeAttachmentChapSecrets" {
+#  value = ["${oci_core_volume_attachment.TFBlockAttach.*.chap_secret}"]
+#}
+
