795b7631419861466811d7316b3e3ceb828e3566
Todd Sharp <toddraymondsharp@gmail.com>
2019-01-04 16:43:07 -0500

    update Jenkinsfile

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,3 +6,13 @@ provider "oci" {
   private_key_path = "${var.private_key_path}"
   region = "${var.region}"
 }
+
+provider "oci" {
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  private_key_password = "${var.private_key_password}"
+  fingerprint = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region = "us-ashburn-1"
+  alias = "oci-ash"
+}
