a46346b3b0012fe8d758e0d41545d770bd3bc6ab
Todd Sharp <toddraymondsharp@gmail.com>
2019-01-04 13:45:13 -0500

    update Jenkinsfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-variable "compartment_ocid" {}
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "private_key_password" {}
-variable "region" {}
 
 provider "oci" {
   tenancy_ocid = "${var.tenancy_ocid}"
