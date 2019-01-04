84f73cb62652896040402a20c45159155afd312f
Todd Sharp <toddraymondsharp@gmail.com>
2019-01-04 13:56:33 -0500

    update Jenkinsfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,8 @@
-
-
-data "oci_identity_availability_domains" "ads" {
-  compartment_id = "${var.compartment_ocid}"
+provider "oci" {
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  private_key_password = "${var.private_key_password}"
+  fingerprint = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region = "${var.region}"
 }
-
-data "oci_containerengine_clusters" "test_clusters" {
-    compartment_id = "${var.compartment_ocid}"
-}
-
-# Output the result
-output "show-ads" {
-  value = "${data.oci_identity_availability_domains.ads.availability_domains}"
-}
-
-output "test" {
-  value = "${data.oci_identity_availability_domains.ads.availability_domains.0.compartment_id}"
-}
-
-output "test2" {
-  value = "${data.oci_containerengine_clusters.test_clusters.clusters}"
-}
-
